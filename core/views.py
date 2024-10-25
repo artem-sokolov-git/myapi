e6f162410f24a9edd5af54b2dfe079ac74df530a
proletsoz <proletsoz@gmail.com>
2024-10-25 17:50:18 -0400

    JsonRenderer added.

--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
 from rest_framework import generics
+
+# from rest_framework.renderers import JSONRenderer
 from .models import Task
 from .serializers import TaskSerializer
 
@@ -6,8 +8,10 @@ from .serializers import TaskSerializer
 class TaskListCreate(generics.ListCreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    # renderer_classes = [JSONRenderer]
 
 
 class TaskListUpdate(generics.RetrieveUpdateDestroyAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    # renderer_classes = [JSONRenderer]
